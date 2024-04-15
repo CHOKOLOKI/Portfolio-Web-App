ad8898cd39e0aa581044eb72f4a5f4e8cb41e799
Sergio Daguil <daguilgio@gmail.com>
2024-04-15 19:37:40 +0800

    added image and design with animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,26 +1,81 @@
-import React from 'react'
-import Image from 'next/image';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section>
-            <div className='grid grid-cols-1 lg:grid-cols-12'>
-          <div className='col-span-7 place-self-center'>
-              <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I am Sergio Daguil III</h1>
-                <p className='text-[#ADB7BE] text-lg lg:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dicta asperiores repellendus nemo quod eligendi, 
-                      consectetur iusto aliquid recusandae dolor in labore ipsum sapiente fugiat odit numquam omnis temporibus non.</p>
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-teal-400">
+              Hello, I'm{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Sergio Daguil III",
+                1000,
+                "Web Developer",
+                1000,
+                "Front-end Developer",
+                1000,
+                "Manual QA Tester",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-sky-400 to-teal-400 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-sky-400 to-teal-400 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
           </div>
-          <div className='col-span-5'>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] mx-10 sm:my-11 relative">
             <Image
-              src={`/images/hero-icon.png`}
-              alt="Hero icon"
+              src="/images/profile.jpeg"
+              alt="profile"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               width={300}
               height={300}
             />
           </div>
+        </motion.div>
       </div>
     </section>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
